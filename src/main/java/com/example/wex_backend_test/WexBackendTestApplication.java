1e6e1d13172dcfa07fd16d44bc642b51b6ed65a7
gabriel piovani <gabrielpiovani@mangobits.com>
2023-09-23 00:50:12 -0300

    INNITITAL COMMIT

---
+++ src/main/java/com/example/wex_backend_test/WexBackendTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.wex_backend_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WexBackendTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WexBackendTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wex_backend_test/WexBackendTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wex_backend_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WexBackendTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
